3ab3a85f5212bc9917026569f97125feb6f78dbe
XiomaraHuamani <xiomara.huamani.c@gmail.com>
2024-06-01 20:54:48 -0500

    De ts a js

---
+++ src/pages/pages/Ingresos/registroIngresos.vue
@@ -0,0 +1,27 @@
+<script setup>
+import * as demoCode from '../../../views/demos/forms/tables/data-table/demoCodeDataTable';
+</script>
+
+<template>
+  <VRow>
+    <VCol cols="12">
+      <!-- 👉 Basic -->
+      <AppCardCode title="Basic" :code="demoCode.basic" no-padding>
+        <DemoDataTableBasic />
+      </AppCardCode>
+    </VCol>
+    <!-- 👉 External Pagination  -->
+    <VCol cols="12">
+      <AppCardCode title="External Pagination" :code="demoCode.externalPagination" no-padding>
+        <DemoDataTableExternalPagination />
+      </AppCardCode>
+    </VCol>
+
+    <!-- 👉 Kitchen Sink  -->
+    <VCol cols="12">
+      <AppCardCode title="Kitchen Sink" :code="demoCode.kitchenSink" no-padding>
+        <DemoDataTableKitchenSink />
+      </AppCardCode>
+    </VCol>
+  </VRow>
+</template>
